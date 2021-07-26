eb817f8f1830716d3a5b0f3fce90ea7bb682fdfd
cameronjoy <camlangdev@gmail.com>
2021-07-26 16:21:28 -0400

    working todo list before login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
-class User(db.Model):
+class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     # user = db.Column(db.String(80))
     # username = db.Column(db.String(80), unique=True)
@@ -19,9 +19,22 @@ class User(db.Model):
         return '<Task %r>' % self.id
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def home():
-    return render_template('index.html')
+    if request.method =='POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            print('error!')
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
 
 if __name__ == '__main__':
     app.run(debug=True)
